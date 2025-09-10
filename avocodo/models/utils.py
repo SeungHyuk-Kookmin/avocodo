da885a973a61b17641909c07e3fec05c06ee57e5
SeungHyuk_0276 <105186530+SeungHyuk-Kookmin@users.noreply.github.com>
2025-09-10 19:18:26 +0900

    Update utils.py

--- avocodo/models/utils.py
+++ avocodo/models/utils.py
@@ -1,18 +1,33 @@
 # -*- coding: utf-8 -*-
-
 # Copyright 2020 Jungil Kong
-#  MIT License (https://opensource.org/licenses/MIT)
+# MIT License
+"""
+HiFi-GAN util 함수 모음
+"""
 
-"""."""
-'''
-Copied from https://github.com/jik876/hifi-gan/blob/master/utils.py
-'''
 
 def get_padding(kernel_size, dilation=1):
-    return int((kernel_size*dilation - dilation)/2)
+    """
+    Conv1d / Conv2d 등에서 padding 값을 자동 계산하는 함수
+    - kernel_size: 커널 크기
+    - dilation: dilation factor
+
+    공식: padding = ((kernel_size * dilation) - dilation) / 2
+      → receptive field가 중앙 정렬되도록 맞추는 역할
+    """
+    return int((kernel_size * dilation - dilation) / 2)
 
 
 def init_weights(m, mean=0.0, std=0.01):
+    """
+    Conv 레이어 초기화 함수
+    - m: layer 모듈
+    - mean: 평균
+    - std: 표준편차
+
+    Conv 계열(layer 이름에 "Conv"가 들어가는 경우) weight를
+    N(mean, std^2) 정규분포로 초기화.
+    """
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
         m.weight.data.normal_(mean, std)
